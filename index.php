4e4860f800d22388f19ab7d13b6cb5132e3027da
Amanda Rydstern <arydstern@gmail.com>
2015-09-15 09:44:43 +0200

    första committen

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Amanda</title>
+		<link href="wp-content/themes/amanda/style.css" rel="stylesheet" type="text/css">
+		<meta name"description" content""/>
+		<meta charset="utf-8"/>		
+		</head>
+	<body>
+		<h1>Amanda</h1>
+		<p>Hej på er</p>
+	</body>	
+</html>
